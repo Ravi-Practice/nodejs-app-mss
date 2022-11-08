36b9f287ad535fb0fa9f7421df9f0fe6ff1eccb4
ravitechdevops <114498256+ravitechdevops@users.noreply.github.com>
2022-11-08 22:28:46 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.33.186:9000/',
+  serverUrl: 'http://172.31.1.135:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    //'sonar.login': 'squ_7d24dd1e8ab896e1048111eefabd496b15c667e0',
 	    'sonar.login': 'admin',
-	    'sonar.password': 'Physics@29',
+	    'sonar.password': 'admin123',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
